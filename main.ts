aa56634ac8ba9207a447ec0ace2d4b3f2fc400cb
warre-rgb <warre@denheuvel.be>
2026-01-12 13:21:22 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 basic.forever(function () {
-	
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 106)
+    basic.pause(785)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 47)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 47)
+    basic.pause(650)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 106)
 })
